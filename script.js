bfaa629401a49bfec289cc77cf6246216dc3596f
NickSessa <nicholas.sessa@gmail.com>
2021-03-10 19:23:41 -0500

    Added ability to click on the timeline

--- script.js
+++ script.js
@@ -122,8 +122,19 @@ function updateProgressBar(e) {
     }
 }
 
+// set Progress Bar
+function setProgressBar(e) {
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const { duration } = music;
+    music.currentTime = (clickX / width) * duration;
+}
+
 // Event Listeners
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
 
+music.addEventListener('ended', nextSong);
+
 music.addEventListener('timeupdate', updateProgressBar);
+progressContainer.addEventListener('click', setProgressBar);
